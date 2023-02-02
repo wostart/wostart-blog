1ff6b88fe9a197a5d305daa3c8181286c191e313
wostart <worldsmart@qq.com>
2023-02-02 17:34:55 +0800

    初始化项目

---
+++ src/test/java/com/example/blog/WostartBlogApplication.java
@@ -0,0 +1,12 @@
+package com.example.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WostartBlogApplication {
+
+    @Test
+    void contextLoads() {
+    }
+}
